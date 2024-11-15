4f5e95908a64ee4cb52a87d7d73bc42ed4d801cc
Jeff Huang <jeffishjeff@gmail.com>
2024-11-15 22:48:08 +0700

    more boilerplates

--- src/LvrMinimizingHook.sol
+++ src/LvrMinimizingHook.sol
@@ -1,8 +1,71 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.8.28;
 
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {ILiquidityPool} from "./interfaces/ILiquidityPool.sol";
+import {PoolState} from "./types/PoolState.sol";
 import {BaseHook} from "./BaseHook.sol";
 
 /// @title LvrMinimizingHook
-contract LvrMinimizingHook is ILiquidityPool, BaseHook {}
+contract LvrMinimizingHook is ILiquidityPool, BaseHook {
+    error OnlyPoolManager();
+    error InvalidHookAddress();
+    error OnlyInitializeViaHook();
+    error OnlyAddLiquidityViaHook();
+    error OnlyRemoveLiquidityViaHook();
+
+    IPoolManager private immutable poolManager;
+    mapping(PoolId => PoolState) private poolStates;
+
+    modifier onlyPoolManager() {
+        require(msg.sender == address(poolManager), OnlyPoolManager());
+        _;
+    }
+
+    constructor(IPoolManager poolManager_) {
+        uint160 mask = Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+            | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG;
+        require(uint160(address(this)) & mask == mask, InvalidHookAddress());
+
+        poolManager = poolManager_;
+    }
+
+    function beforeInitialize(address sender, PoolKey calldata, uint160)
+        external
+        view
+        override
+        onlyPoolManager
+        returns (bytes4)
+    {
+        require(sender == address(this), OnlyInitializeViaHook());
+
+        return this.beforeInitialize.selector;
+    }
+
+    function beforeAddLiquidity(
+        address sender,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external view override onlyPoolManager returns (bytes4) {
+        require(sender == address(this), OnlyAddLiquidityViaHook());
+
+        return this.beforeAddLiquidity.selector;
+    }
+
+    function beforeRemoveLiquidity(
+        address sender,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external view override onlyPoolManager returns (bytes4) {
+        require(sender == address(this), OnlyRemoveLiquidityViaHook());
+
+        return this.beforeRemoveLiquidity.selector;
+    }
+
+    // TODO: before/after swap
+}
