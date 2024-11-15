40c9a3804f7f569055dd93ce6728b5ff53c96c27
Jeff Huang <jeffishjeff@gmail.com>
2024-11-15 22:08:51 +0700

    base hook

---
+++ src/BaseHook.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.8.28;
+
+import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {BeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+/// @title BaseHook
+abstract contract BaseHook is IHooks {
+    error HookNotImplemented();
+
+    /// @inheritdoc IHooks
+    function beforeInitialize(address, PoolKey calldata, uint160) external virtual returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function afterInitialize(address, PoolKey calldata, uint160, int24) external virtual returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function beforeAddLiquidity(address, PoolKey calldata, IPoolManager.ModifyLiquidityParams calldata, bytes calldata)
+        external
+        virtual
+        returns (bytes4)
+    {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function afterAddLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external virtual returns (bytes4, BalanceDelta) {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function beforeRemoveLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function afterRemoveLiquidity(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) external virtual returns (bytes4, BalanceDelta) {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function beforeSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, bytes calldata)
+        external
+        virtual
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function afterSwap(address, PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        virtual
+        returns (bytes4, int128)
+    {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function beforeDonate(address, PoolKey calldata, uint256, uint256, bytes calldata)
+        external
+        virtual
+        returns (bytes4)
+    {
+        revert HookNotImplemented();
+    }
+
+    /// @inheritdoc IHooks
+    function afterDonate(address, PoolKey calldata, uint256, uint256, bytes calldata)
+        external
+        virtual
+        returns (bytes4)
+    {
+        revert HookNotImplemented();
+    }
+}
